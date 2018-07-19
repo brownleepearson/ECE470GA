a8d068b1d903cc1b9c9b3caac0e0d39c6b74dbb6
marswon0 <41454852+marswon0@users.noreply.github.com>
2018-07-19 16:55:01 -0700

    Add files via upload

--- project.m
+++ project.m
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-
-clear all
-load('X1200.mat')
-mu=[];
-U=[];
-Uq_test=[];
-m=1200;
-n=10;
-
-for i=1:10
-   x = X1200(:,m*(i-1)+1:m*i);
-   avg = mean(x,2);
-   Xh = x-avg;
-   C= Xh*Xh';
-   [u,~] = eigs(C,n);
-   U = [U u];
-   mu = [mu avg];
-end
-
-% U has a total of 10*n columns 
-
-for f = 1:10
-    for g=1:10
-    number = n*(f-1)+1+randperm(n-1,1);
-    Uq_test = [Uq_test U(:,number)]; 
-    end
-end
-
-t0 = cputime;
-Classify(U,mu,n);
-t1 = cputime -t0
-
-=======
 
 clear all
 load('X1200.mat')
@@ -40,7 +6,7 @@ U=cell(1,10);
 mu=cell(1,10);
 Uq_test=[];
 m=1200;
-n=40;
+n=200;
 
 for i=1:10
    x = X1200(:,m*(i-1)+1:m*i);
@@ -52,8 +18,9 @@ for i=1:10
    mu{i} = avg;
 end
 
-t0=cputime;
-Classify(U,mu);
-t1 = cputime-t0
+A = generate_feature(U);
 
->>>>>>> 1db4a888c5f07d43fb414eda34a51b135cbbf8d0
+% t0=cputime;
+% Classify(U,mu);
+% t1 = cputime-t0
+
