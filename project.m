186299cebd25a47f499d92cb65e0d58ac03a4f73
Bob Lin <boblin@uvic.ca>
2018-07-31 15:40:50 -0700

    generate feature from individual

--- project.m
+++ project.m
@@ -1,7 +1,7 @@
 
 clear all
 load('X1200.mat')
-feature_library=cell(1,10);%init the container for feature vector
+fea_library=cell(1,10);%init the container for feature vector
 mu=cell(1,10);%calculate mean of the training data
 Uq_test=[];
 m=1200;%m is the size of each class in the training data
@@ -13,10 +13,12 @@ for i=1:10
    Xh = x-avg;
    C= Xh*Xh';
    [u,~] = eigs(C,n);
-   feature_library{i}= u;             
+   fea_library{i}= u;             
    mu{i} = avg;
 end
 population = gen_population;  %create a population 
+individual= population{1};
+feature = gen_feature(fea_library,individual);
 
 % t0=cputime;
 %Classify(U,mu);
